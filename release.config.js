3a99d9da3e749bd39c9b0f59b8326d7d11a48ef6
JungYoon Cha <jamiecha@gmail.com>
2021-06-09 11:37:56 +0900

    fix: fixed style of release config

--- release.config.js
+++ release.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    branches: "master",
-    repositoryUrl: "https://github.com/jamiecha/react-app",
-    plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/github'  // Create a github release
-    ]
-}
+  branches: "master",
+  repositoryUrl: "https://github.com/jamiecha/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github", // Create a github release
+  ],
+};
