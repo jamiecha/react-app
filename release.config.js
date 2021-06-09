5129d47e662936562ea1d37b6cc4aa2cec83a73e
JungYoon Cha <jamiecha@gmail.com>
2021-06-09 11:35:41 +0900

    fix: fixed plugin name of release.config.js

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@ module.exports = {
     branches: "master",
     repositoryUrl: "https://github.com/jamiecha/react-app",
     plugins: [
-        '@semantic-release/commit-analyser',
+        '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         '@semantic-release/github'  // Create a github release
     ]
