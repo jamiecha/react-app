1ad95f7f43558ba5244d198c94729d89341c6e37
JungYoon Cha <jamiecha@gmail.com>
2021-06-10 14:36:14 +0900

    fix: more core.things()

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -618,14 +618,26 @@ const core = __webpack_require__(470)
 const github = __webpack_require__(469)
 
 try {
-    throw new Error("some error message")
+    // throw new Error("some error message")
+
+    core.debug('Debug message')
+    core.warning('Warning message')  // yellow
+    core.error('Error message')  // red
+
     const name = core.getInput('who-to-greet')
+    core.setSecret(name)  // name will be hidden
+
     console.log(`Hello ${name}`)
 
     const time = new Date()
     core.setOutput("time", time.toTimeString())
 
+    core.startGropu('Logging github object')
     console.log(JSON.stringify(github, null, '\t'))
+    core.endGroup()
+
+    core.exportVariable('HELLO', 'hello')
+
 } catch(error) {
     core.setFailed(error.message)
 }
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,14 +2,26 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 try {
-    throw new Error("some error message")
+    // throw new Error("some error message")
+
+    core.debug('Debug message')
+    core.warning('Warning message')  // yellow
+    core.error('Error message')  // red
+
     const name = core.getInput('who-to-greet')
+    core.setSecret(name)  // name will be hidden
+
     console.log(`Hello ${name}`)
 
     const time = new Date()
     core.setOutput("time", time.toTimeString())
 
+    core.startGropu('Logging github object')
     console.log(JSON.stringify(github, null, '\t'))
+    core.endGroup()
+
+    core.exportVariable('HELLO', 'hello')
+
 } catch(error) {
     core.setFailed(error.message)
 }
