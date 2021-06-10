23a7904cb3eb0703d6c89db73d18bfa8183d2785
JungYoon Cha <jamiecha@gmail.com>
2021-06-10 15:32:46 +0900

    fix: async/await

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -3623,25 +3623,31 @@ exports.getState = getState;
 const core = __webpack_require__(470)
 const github = __webpack_require__(469)
 
-try {
-    const token = core.getInput('token')
-    const title = core.getInput('title')
-    const body = core.getInput('body')
-    const assignees = core.getInput('assignees')
+async function run() {
+    try {
+        const token = core.getInput('token')
+        const title = core.getInput('title')
+        const body = core.getInput('body')
+        const assignees = core.getInput('assignees')
 
-    const octokit = new github.GitHub(token)
-    const response = octokit.issues.create({
-        ...github.context.repo,  // repo.owner, repo.repo
-        title,
-        body,
-        assignees: assignees ? assignees.split('\n') : undefined
-    })
+        // const octokit = new github.GitHub(token)
+        const octokit = github.getOctokit(token)
 
-    core.setOutput('issue', JSON.stringify(response.data))
+        const response = await octokit.rest.issues.create({
+            ...github.context.repo,  // repo.owner, repo.repo
+            title,
+            body,
+            assignees: assignees ? assignees.split('\n') : undefined
+        })
 
-} catch (error) {
-    core.setFailed(error.message)
+        core.setOutput('issue', JSON.stringify(response.data))
+
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
+
+run()
 
 /***/ }),
 
